7749d9e1e137568014b6043aba1a7c71ed889820
sasegura <sasegura.fernandez87@gmail.com>
2020-09-22 16:59:32 +0200

    dsa

---
+++ build/precache-manifest.190cfc9b3a7d474514a573b76cbffaf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9b0013e3bd52a55463567ece1a51dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae67a6f2b1a45cf47eec",
+    "url": "/static/css/main.f9c94372.chunk.css"
+  },
+  {
+    "revision": "fd7e371e8401ae475eda",
+    "url": "/static/js/2.8137d076.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/static/js/2.8137d076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae67a6f2b1a45cf47eec",
+    "url": "/static/js/main.2997110c.chunk.js"
+  },
+  {
+    "revision": "44bd36f641507fc19dfa",
+    "url": "/static/js/runtime-main.cb15fdae.js"
+  }
+]);
